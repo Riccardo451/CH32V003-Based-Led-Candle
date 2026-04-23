2fff85919bf47098c60e37639fd41eeedff836fd
Riccardo Montaguti <ricky451@hotmail.it>
2026-04-23 17:44:06 +0200

    Refactor LED control to use defined pin array

--- engine/main.ino
+++ engine/main.ino
@@ -1,14 +1,28 @@
+// Arduino sketch
+
+#include <Arduino.h>
 #include "candle.h"
 
+// choose 3 PWM-capable pins
+const uint8_t pins[3] = {3, 5, 6};
+
+// required by candle.h
 void hw_set_led(uint8_t ch, uint8_t value) {
-    analogWrite(ch + 3, value); // pins 3,4,5
+analogWrite(pins[ch], value);
 }
 
 void setup() {
-    candle_init();
+for (int i = 0; i < 3; i++) {
+pinMode(pins[i], OUTPUT);
+}
+
+```
+candle_init();
+```
+
 }
 
 void loop() {
-    candle_update();
-    delay(5);
+candle_update();
+delay(5); // ~200 Hz update rate
 }
